25aaed925bbcaf55f851b43194192a430b3dbf78
josiahwebb <jj.webb520@gmail.com>
2021-12-11 17:49:35 -0700

    adds and prints values from input field in <a>  formate

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
+const listEl = document.getElementById('ul-el')
 let myLeads = []
 
+
+
 inputBtn.addEventListener("click", function() {
-    myLeads.push('inputEl')
-    console.log(myLeads)
+    myLeads.push(inputEl.value)
+    printLeads()
+    inputEl.value = ''
 })
+
+
+function printLeads () {
+    let listItems = ''
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`
+    }
+    listEl.innerHTML = listItems
+}
